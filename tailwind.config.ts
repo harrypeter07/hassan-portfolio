256409acab12be81e1285b3e7525df59156aae91
harrypeter07 <hassanmansuri570@gmail.com>
2024-12-13 20:51:14 +0530

    commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,19 @@
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/slices/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [
+  require('@tailwindcss/typography') // Use the imported plugin directly
+  ],
+};
+
+export default config;
