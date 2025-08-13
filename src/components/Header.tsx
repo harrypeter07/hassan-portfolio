8e1ce2b01487f776bf68f8956a5e4d1a329769e2
Hassan Mansuri <hassanmansuri570@gmail.com>
2025-08-13 08:08:40 +0530

    wwasjhbuh

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { createClient } from "@/prismicio";
+import { getSettingsData } from "@/lib/data";
 import NavBar from "./Navbar";
 
-
 export default async function Header() {
-  const client = createClient();
-  const settings = await client.getSingle("settings");
+  const settings = await getSettingsData();
   return (
     <header className="top-0 z-50 mx-auto max-w-7xl md:sticky md:top-4">
       <NavBar settings={settings} />
