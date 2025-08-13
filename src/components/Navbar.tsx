4d3a79d8e9d628003b22972f53899bfc86c43151
Hassan Mansuri <hassanmansuri570@gmail.com>
2025-08-13 08:13:01 +0530

    s

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,7 +20,7 @@ export default function NavBar({
     <nav aria-label="Main navigation">
       <ul className="flex flex-col justify-between rounded-b-lg bg-slate-50 px-4 py-2 md:m-4 md:flex-row md:items-center md:rounded-xl">
         <div className="flex items-center justify-between">
-          <NameLogo name={settings.data.name} />
+          <NameLogo name={settings.name} />
           <button
             aria-expanded={open}
             aria-label="Open menu"
